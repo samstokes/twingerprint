1de801f03f38711f07bf386c225ce0965f5a7d82
Sam Stokes <sam@35cr.sc.ss496.co.uk>
2009-03-30 00:18:47 +0100

    Vendorise gem httparty-0.3.1

--- config/environment.rb
+++ config/environment.rb
@@ -7,4 +7,5 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'httparty', :version => '>= 0.3.1'
 end
