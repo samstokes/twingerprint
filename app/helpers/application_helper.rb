a962e993db9159a269e0a68176f50967ccd29231
Sam Stokes <sam@35cr.sc.ss496.co.uk>
2009-03-29 20:45:28 +0100

    App layout, with stylesheet and page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def page_title(text)
+    content_for(:title) { text }
+    content_tag(:h1, text)
+  end
 end
