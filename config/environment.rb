d80837d5e46d48b559524d5196e4f04e1bfc269e
Sam Stokes <sam@35cr.sc.ss496.co.uk>
2009-04-09 01:02:20 +0100

    Disable ActiveRecord for now...

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.frameworks -= [:active_record]
+
   config.gem 'httparty', :version => '>= 0.3.1'
 end
